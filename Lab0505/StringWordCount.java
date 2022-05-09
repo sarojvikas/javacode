13929760a1706d599412da9143509ed0b09660a1
sarojvikas <103246202+sarojvikas@users.noreply.github.com>
2022-05-09 16:29:48 +0530

    Create StringWordCount.java

---
+++ Lab0505/StringWordCount.java
@@ -0,0 +1,52 @@
+package Lab0505;
+
+
+
+import java.util.Scanner;
+
+
+
+public class StringWordCount {
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc =new Scanner(System.in);
+
+		System.out.println("Enter a Senetence");
+
+		String sentence = sc.nextLine();
+
+		System.out.println("Enter a word for checking");
+
+		String word = sc.next();
+
+		int count =0;
+
+		for(int i=0;i<sentence.length();i++) {
+
+			if(sentence.indexOf(word,i)==i) {
+
+				count+=1;
+
+				
+
+			}
+
+			
+
+		}
+
+		System.out.println("Word occured in String is: "+count);
+
+		
+
+
+
+	}
+
+
+
+}
