62bc512b79153f68f187ae8a739062f5d5c7c02e
sarojvikas <103246202+sarojvikas@users.noreply.github.com>
2022-07-01 12:36:09 +0530

    Create ElementGreaterThanLastLimitException.java

---
+++ lab3006/ElementGreaterThanLastLimitException.java
@@ -0,0 +1,12 @@
+package lab3006; 
+  
+ public class ElementGreaterThanLastLimitException extends Exception{ 
+  
+         public ElementGreaterThanLastLimitException() { 
+                 super(); 
+                 System.out.println("Element should be less than last limit value"); 
+         } 
+          
+          
+  
+ }
