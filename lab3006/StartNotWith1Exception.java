0a20e311e0199285489fcc5e353c1f66e745eafe
sarojvikas <103246202+sarojvikas@users.noreply.github.com>
2022-07-01 12:40:35 +0530

    Create StartNotWith1Exception.java

---
+++ lab3006/StartNotWith1Exception.java
@@ -0,0 +1,19 @@
+package lab3006; 
+
+  
+
+ public class StartNotWith1Exception extends Exception { 
+
+  
+
+         public StartNotWith1Exception() { 
+
+                 super(); 
+
+                 System.out.println("Series should start with 1"); 
+
+         } 
+
+          
+
+ }
