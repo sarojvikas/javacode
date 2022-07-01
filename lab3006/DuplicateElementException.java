806825603236641eee8d9475b5325c4274e26420
sarojvikas <103246202+sarojvikas@users.noreply.github.com>
2022-07-01 12:34:51 +0530

    Create DuplicateElementException.java

---
+++ lab3006/DuplicateElementException.java
@@ -0,0 +1,11 @@
+package lab3006; 
+  
+ public class DuplicateElementException extends Exception { 
+  
+         public DuplicateElementException() { 
+                 super(); 
+                 System.out.println("Elements should not repeat"); 
+         } 
+  
+          
+ }
